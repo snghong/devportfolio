5a83e5aa2ba233461030a77bac7c1f4454baebb3
Sng Hong <sng_hong@outlook.com>
2021-04-13 16:23:30 +0800

    debug

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,18 +9,19 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hi! My name is ',
+  name: 'Sng Hong',
+  subtitle: "I'm a Software Developer",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  img: 'profile.jpeg',
+  paragraphOne: "Hi there! I'm Sng Hong, a software developer from Singapore.",
+  paragraphTwo:
+    'My projects include: Game Development with C# and Unity, Web Development with React, and UI/UX for E-Commerce. My stack includes: C#, Python, HTML5, CSS3, JavaScript and React.',
+  paragraphThree: "Carnegie Mellon University '25",
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -28,29 +29,43 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
+    img: 'novena image.png',
+    title: 'Novena Furniture Singapore',
+    info:
+      'As the Front-End Developer for their E-Commerce website, I focused on Mobile Optimization, Responsive Design and building utility features for consumers.',
+    info2: 'User Interface/Experience design with Shopify Liquid and SCSS.',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+
+  {
+    id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    title: 'E-Commerce Project with Nextjs',
+    info: 'Fully functional E-Commerce site built with React, Nextjs and styled with Tailwind.',
+    info2:
+      'Uses Airtable for database management and a serverless lambda function for payment integration at checkout.',
     url: '',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
+    img: 'project sushi 1.png',
+    title: 'Project Sushi',
+    info:
+      "In Project Sushi, you control a tiny human with a large craving for salmon sushi! Help him navigate various perils including the conveyor belt, onerous onigiri, monstrous maki, and even chopsticks from above whose owners think that he's a food item as well! ",
+    info2: 'Light-hearted arcade game built with Unity 2D and C# scripting.',
+    url: 'https://red-bean.itch.io/project-sushi',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
+    img: 'HomeboundSS1.PNG',
+    title: 'Homebound',
+    info:
+      'Aliens have invaded your homeland, leaving destruction in their wake. Make the perilous journey home, and restore peace to your beloved night sky.',
+    info2: 'Intergalactic space chase built with Unity 3D, C# scripting, and animated using Unity.',
+    url: 'https://red-bean.itch.io/homebound',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
 ];
@@ -59,7 +74,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'sng_hong@outlook.com',
 };
 
 // FOOTER DATA
@@ -67,28 +82,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'www.linkedin.com/in/sng-hong',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/snghong',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || "Sng Hong's Portfolio"}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || "Sng Hong's Portfolio"} />
       </Helmet>
       <App />
     </>
